a473d8b2c399f66eb16b4061149a52fbdc0214d4
Denis Dubovikov <denisdubovikov96@gmail.com>
2020-10-28 15:34:54 +0200

    styles fix

--- src/components/Board/Cell.js
+++ src/components/Board/Cell.js
@@ -5,54 +5,25 @@ import { booom, bombIcon, flag, stopBoom } from "../../statik";
 export default function Cell({ item, isGameOver }) {
   const { neighborMineCount, isOpen, isMined, isFlagged, id } = item;
 
-  const cls = classNames("cell", { cellOpen: isOpen });
-  const fontColor = (count) => {
-    switch (count) {
-      case 0:
-      case 1:
-        return "#039be5";
-      case 2:
-      case 3:
-        return "#ef6c00";
-      case 4:
-      case 5:
-      case 6:
-      case 7:
-      case 8:
-        return "#ff3d00";
-      default:
-        return "white";
-    }
-  };
   if (isMined && isGameOver) {
+    const cls = classNames("cell", {
+      cellOpen: isOpen,
+      cellOpenMined: isMined && isOpen,
+      cellSaved: isMined && isFlagged,
+    });
+    const src = isOpen ? booom : isMined && isFlagged ? stopBoom : bombIcon;
     return (
-      <div
-        data-id={id}
-        className={cls}
-        style={{
-          background:
-            isMined && isOpen ? "#d32f2f" : isMined && isFlagged ? "white" : "",
-          boxShadow: isMined && isOpen ? "0px 0px 15px 3px #d32f2f" : "",
-          zIndex: isMined && isOpen ? 1500 : "",
-          border: isMined && isOpen ? "none" : "",
-        }}
-      >
-        {isOpen ? (
-          <img src={booom} alt="aa" />
-        ) : isMined && isFlagged ? (
-          <img src={stopBoom} alt="aa" />
-        ) : (
-          <img src={bombIcon} alt="aa" />
-        )}
+      <div data-id={id} className={cls}>
+        <img src={src} alt="aa" />
       </div>
     );
   } else {
+    const cls = classNames("cell", `cellColor-${neighborMineCount}`, {
+      cellOpen: isOpen,
+      cellOpenMined: isMined && isOpen,
+    });
     return (
-      <div
-        data-id={id}
-        className={cls}
-        style={{ color: fontColor(neighborMineCount) }}
-      >
+      <div data-id={id} className={cls}>
         {isOpen ? (
           neighborMineCount !== 0 ? (
             neighborMineCount
